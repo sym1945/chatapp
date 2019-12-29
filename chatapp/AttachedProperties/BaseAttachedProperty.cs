2f81c1e862ffd56bc6ada1de2c6a46229393dcc0
sym1945 <sym1945@gmail.com>
2019-12-29 14:44:24 +0900

    add AttachedProperties

---
+++ chatapp/AttachedProperties/BaseAttachedProperty.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+
+namespace chatapp
+{
+    public abstract class BaseAttachedProperty<Parent, Property>
+        where Parent : BaseAttachedProperty<Parent, Property>, new()
+    {
+        #region Public Events
+
+        public event Action<DependencyObject, DependencyPropertyChangedEventArgs> ValueChanged = (sender, e) => { };
+
+        #endregion
+
+        #region Public Properties
+
+        public static Parent Instance { get; private set; } = new Parent();
+
+        #endregion
+
+        #region Attached Property Definitions
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.RegisterAttached(
+                "Value"
+                , typeof(Property)
+                , typeof(BaseAttachedProperty<Parent, Property>)
+                , new PropertyMetadata(new PropertyChangedCallback(OnValuePropertyChanged))
+            );
+
+        /// <summary>
+        /// <see cref="ValueProperty"/>가 변경됐을 때 콜백되는 이벤트
+        /// </summary>
+        /// <param name="d">Value Property를 갖고있는 UI 엘레먼트</param>
+        /// <param name="e">이벤트 아규먼트</param>
+        private static void OnValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            // Parent function 호출
+            Instance.OnValueChanged(d, e);
+
+            // 이벤트 리스터 호출
+            Instance.ValueChanged(d, e);
+        }
+
+        public static Property GetValue(DependencyObject d) => (Property)d.GetValue(ValueProperty);
+
+        public static void SetValue(DependencyObject d, Property value) => d.SetValue(ValueProperty, value);
+
+
+        #endregion
+
+        #region Event Methods
+
+        public virtual void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e) { }
+
+        #endregion
+    }
+
+}
---
+++ chatapp/ValueConverters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace chatapp
+{
+    public class BooleanToVisibilityConverter : BaseValueConverter<BooleanToVisibilityConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
