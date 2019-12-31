d40da2fcd27ef110bbd880e79b482cac23181d63
sym1945 <sym1945@gmail.com>
2019-12-31 18:13:15 +0900

    create Chat Message Bubbles

---
+++ chatapp/Controls/Chat/ChatMessage/ChatMessageListControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace chatapp
+{
+    /// <summary>
+    /// ChatMessageListControl.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ChatMessageListControl : UserControl
+    {
+        public ChatMessageListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
